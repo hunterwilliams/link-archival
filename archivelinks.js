434561cd50e4b06c0bfbcd99c22f2fe9e32471e9
Hunter Williams <hunter@huntergwilliams.com>
2020-06-03 11:05:20 +0700

    puts scraping in parallel

--- archivelinks.js
+++ archivelinks.js
@@ -1,6 +1,7 @@
 const fs = require('fs'),
     es = require('event-stream'),
     os = require('os');
+const { fork } = require('child_process');
 const puppeteer = require('puppeteer');
 
 // returns an array of URLS based on a file with links
@@ -41,18 +42,34 @@ function makeDirIfMissing(path) {
   }
 }
 
-
-const regexSlash = new RegExp("\/", 'g');
-async function scrape(link, storagePath) {
-
+async function handleJobs(threadName) {
   const browser = await puppeteer.launch({
     args: ['--no-sandbox', '--disable-setuid-sandbox']
   });
+  process.on("message", (msg) => {
+    if (msg === "off") {
+      console.log(`Thread ${threadName} good night`);
+      process.exit(0);
+    } else if (msg.link && msg.storagePath) {
+      scrape(browser, msg.link, msg.storagePath, threadName).then(() => {
+        process.send("next");
+      }).catch((reason) => {
+        console.warn(reason);
+        process.send("error")
+      });
+    }
+  });
+  return true;
+}
+
+
+const regexSlash = new RegExp("\/", 'g');
+async function scrape(browser, link, storagePath, threadName) {
 
   const page = await browser.newPage();
   const fileStorageName = storagePath + link.replace("http://", "").replace("https://","").replace(regexSlash,"_");
 
-  console.log("Puppeteer about to snapshot: " + link);
+  console.log(`Thread ${threadName}: Puppeteer about to snapshot: ${link}`);
   await page.goto(link);
   await page.screenshot({path: fileStorageName + ".png"});
   return true;
@@ -74,7 +91,18 @@ if (process.argv.length < 3) {
     case "links-folder":
       console.log(getLinksFromMdFilesInDirectory('./docs/'));
       break;
+    case "take-jobs":
+      const threadName = process.argv[3] || "";
+      handleJobs(threadName).then(
+        () => {
+          process.send("ready");
+        }
+      ).catch(
+        (reason) => console.warn(`some issue with thread ${threadName}.. `, reason)
+      );
+      break;
     case "screen-all":
+      const maxWorkers = process.argv[3] || 2;
       console.log("Getting map of links from each file");
       const mapOfLinks = getLinksFromMdFilesInDirectory('./docs/');
       console.log("Map of links assembled");
@@ -82,9 +110,7 @@ if (process.argv.length < 3) {
       const outputPath = './output/';
       makeDirIfMissing(outputPath);
       console.log("Ensured parent output directory exists");
-      const totalItemsCount = Object.keys(mapOfLinks).length;
-      let totalFinishedCount = 0;
-
+      const jobs = [];
       for (const item in mapOfLinks) {
         const storagePath = outputPath + item.replace("\.md", "") + "\/";
         makeDirIfMissing(storagePath);
@@ -94,19 +120,40 @@ if (process.argv.length < 3) {
         for (let i = 0; i < mapOfLinks[item].length; i += 1) {
           const link = mapOfLinks[item][i];
           console.log(link);
-          promises.push(scrape(link, storagePath));
+          jobs.push({link, storagePath});
         }
-        Promise.all(promises).then(() => {
-          totalFinishedCount += 1;
-          console.log("Finished storing items related to " + item);
-
-          console.log(`Finished file ${totalFinishedCount} of ${totalItemsCount}`);
-          if (totalFinishedCount === totalItemsCount) {
-            console.log("!All done!");
-            process.exit(1);
+      }
+      const totalLinks = jobs.length;
+      let totalCompleted = 0;
+      let totalErrors = 0;
+      console.log("Total links to handle: " + totalLinks);
+      console.log(`Initializing workers (${maxWorkers})`);
+      let remainingWorkers = 0;
+      for (let i = 0; i < maxWorkers; i +=1 ) {
+        const forked = fork('archivelinks.js' , ["take-jobs", i]);
+        forked.on('message', (msg) => {
+          if (msg === "next" || msg === "ready" || msg === "error") {
+            if (msg === "ready") {
+              remainingWorkers += 1;
+            }
+            if (msg == "error") {
+              totalErrors += 1;
+            }
+            if (msg === "next" || msg === "error") {
+              totalCompleted += 1;
+              console.log(`Completed ${totalCompleted} of ${totalLinks}; Total Errors: ${totalErrors}`);
+            }
+            if (jobs.length > 0) {
+              forked.send(jobs.pop());
+            } else {
+              forked.send("off");
+              remainingWorkers -= 1;
+              if (remainingWorkers === 0) {
+                console.log(`Finished ${totalLinks} Links; Total Errors: ${totalErrors}`);
+                process.exit(0);
+              }
+            }
           }
-        }).catch((reason) => {
-          console.warn("Some issue: " + reason);
         });
       }
       break;
